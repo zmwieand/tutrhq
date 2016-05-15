6142e4d0c418c103d0d93385f3f201612fcb92c9
Zach Wieand <zmwieand@gmail.com>
2016-05-15 19:39:01 -0400

    added in a mongo client and checking that emails are well formed

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var formidable = require("formidable");
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
 
+/* Use and environment variable for the port */
 app.set('port', (process.env.PORT || 5000));
+
+/* Setup the database */
+MongoClient.connect("mongodb://localhost:27017/tutordb", function(err, db) {
+	if(!err) {
+  		db.createCollection('email', {strict:true}, function(err, collection) {});
+  		db.createCollection('schools', {strict:true}, function(err, collection) {});
+  		console.log("We are connected to Tutordb");
+	}
+});
 
 /* root of Tutor */
 app.get('/', function(req, res) {
@@ -18,18 +29,22 @@ app.post('/subscribe/', function(req, res) {
 		var email = fields.email;
 		// make sure the email is well formed and doesn't already exist
 		// if it does flash an error message
-		console.log(email);
+		if (!wellFormedEmail(email)) {
+			res.end('this was not a well formed email, please try again');
+		}
+		sendEmail(email);
 	});
 	res.sendFile(__dirname + '/html/success.html');
 });
 
+/* What schools are interested in Tutor */
 app.post('/tutor_to/', function(req, res){
 	var form = formidable.IncomingForm();
 	form.parse(req, function(err, fields, files){
 		var school = fields.school;
 		console.log(school);
 	});
-	res.end("thanks for letting us know");
+	res.end("Thanks for letting us know :)");
 });
 
 app.listen(app.get('port'), function () {
@@ -48,3 +63,43 @@ app.listen(app.get('port'), function () {
 // email.setHtml("and easy to do anywhere, even with Node.js");
 
 // sendgrid.send(email);
+
+
+function wellFormedEmail(email) {
+	var arr = email.split("@");
+	if (arr.length != 2) {
+		console.log('invalid email');
+		return false;
+	}
+
+	var domain = arr[1];
+	arr = domain.split('.');
+	if (arr.length != 2) {
+		console.log('invalid domain');
+		return false;
+	}
+
+	// make sure that this is a .edu email
+	var ext = arr[1];
+	if (ext != 'edu') {
+		console.log('this is not a .edu email');
+		return false;
+	}
+
+	return true;
+};
+
+function sendEmail(email) {
+	console.log('We will send an email to: ' + email);
+	/* using SendGrid's Node.js Library */
+
+	// var sendgrid = require("sendgrid")("SENDGRID_APIKEY");
+	// var email = new sendgrid.Email();
+
+	// email.addTo("test@sendgrid.com");
+	// email.setFrom("you@youremail.com");
+	// email.setSubject("Sending with SendGrid is Fun");
+	// email.setHtml("and easy to do anywhere, even with Node.js");
+
+	// sendgrid.send(email);
+};
