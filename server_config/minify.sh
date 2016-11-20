89a61fe32dce923dfb5ad090d57d6cb8a8f68b06
David Dobmeier <daviddob@buffalo.edu>
2016-11-19 20:15:52 -0500

    added script to minify css automatically on server upload

---
+++ server_config/minify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+for file in `find ~/tutrhq.com/app/public/ -name "*.js" -o -name "*.css"`
+do
+#echo "Compressing $file …"
+    if [[ $file != *.min.* ]]
+    then
+	if [[ $file == *.css ]]
+        then
+            echo $file
+            outputname=`echo $file | sed -e 's/\.[^.]*$//'`.min.css
+            type="css"
+        fi
+
+        if [[ $file == *.js ]]
+        then
+            echo $file
+            outputname=`echo $file | sed -e 's/\.[^.]*$//'`.min.js
+            type="js"
+        fi
+        yui-compressor --type $type $file -o $outputname
+    fi
+done
