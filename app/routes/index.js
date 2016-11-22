afb25b1e190557114b82aba96ebe3d3d558c6d60
Tmweppner <tmweppne@buffalo.edu>
2016-11-22 17:28:58 -0500

    updating index.js for searchbar

--- app/routes/index.js
+++ app/routes/index.js
@@ -49,4 +49,44 @@ router.get('/callback',
     res.redirect(req.session.returnTo || '/users');
 });
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var newSchool = new School({
+        instnm: "University at Buffalo"
+    });
+    newSchool.save(function(err) {
+        if (err) throw err;
+    });
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/index', function(req, res, next) {
+    var school_name = req.body['university']
+    // console.log("School: "  + school_name);
+    // console.log("incrementing count");
+    School.findBySchool(school_name, function(err, school) {
+        if (school) {
+          // console.log("Found : " + school.instnm);
+          school.count = school.count + 1;
+          school.save(function(err) {
+              if (err) return err;
+          });
+        }
+
+        res.redirect("/");
+    });
+});
+
+router.post('/index', function(req, res, next) {
+    User.findBySchool(req.body['univeristy'], function(err, school) {
+        if (school) {
+          school.count = school.count +1;
+        }
+        user.save(function(err) {
+            if (err) return err;
+        });
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
